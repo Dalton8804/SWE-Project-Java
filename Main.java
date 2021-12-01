12e38cb81af9e0763ef8f0ab3abd7ca7caace68f
Dalton <Daltonjavery@gmail.com>
2021-11-30 22:07:59 -0600

    updated main

--- Main.java
+++ Main.java
@@ -9,9 +9,25 @@ public class Main {
     public static void onStartUp(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your username: ");
-        String Username = input.nextLine();
+        String Username = input.nextLine();;
         if (Usernames.contains(Username)){
-
+            System.out.println("Hello "+Username+"! Please enter your password :) You have 5 tries!");
+            String Password;
+            int triesLeft = 5;
+            while(true){
+                Password = input.nextLine();
+                if (Password==Username.getPass()) {
+                    System.out.println("Welcome Back!");
+                    break;
+                } else {
+                    triesLeft--;
+                    System.out.println("Password is Incorrect, you have " + triesLeft +" tries left.");
+                }
+            }
+        } else {
+            User.addUser(Username);
+            System.out.println("Please create a password :)");
+            User.setPassword(input.nextLine());
         }
         System.out.println("Username");
     }
