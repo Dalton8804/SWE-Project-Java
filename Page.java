73dcee1c573eef516f302a1883383df7dd8ab0ed
Dalton <Daltonjavery@gmail.com>
2021-12-01 23:50:19 -0600

    Final commit Dec. 1

--- Page.java
+++ Page.java
@@ -1,11 +1,2 @@
-import java.util.HashMap;
 
-public abstract class Page {
-    HashMap<String,String> AppState;
-
-    Page(HashMap<String,String> AppState){ this.AppState = AppState;}
-    
-    public String render(){
-        return "next route";
-    }
-}
+public interface Page{}
