7233f39d8769da1fa740a5572ff03b60c2e1ad08
chandler-barlow <chandlerbrlw@gmail.com>
2021-12-03 21:54:41 -0600

    asdkfj

--- Home.java
+++ Home.java
@@ -1,19 +1,51 @@
 import java.util.HashMap;
+import java.util
 public class Home extends Page {
 
     enum Action {
         LIST,
         GOTOUSER,
+        GOTONEXTPAGE,
+        GOTOPREVPAGE,
         GOTOCAR,
+        EXIT
     }
-    private carListCursor;
 
-    Home(HashMap<String, String> appState){
+    private int carListCursor;
+
+
+    Home(HashMap<String, String> appState, ListOfCars carList, ListOfUsers userList){
         this.carListCursor = 0;
+        this.appState = appState;
+        this.carList = carList;
+        this.userList = userList;
         super(appState);
     }
-    public String
 
+    public String getCarSelection(Scanner input){
+        System.out.println("Please enter a vin:");
+        String vin = input.nextLine();
+        return vin;
+    }
+
+    public Action getAction(int actionCode){
+        if(carListCursor > 0){
+            switch(actionCode){
+                case 0:
+                    return Action.LIST;
+                case 1:
+                    return Action.GOTOUSER;
+                case 2:
+                    return Action.EXIT;
+            }
+        }else{
+            switch(actionCode){
+                case 0:
+                    return
+
+            }
+        }
+    }
 
     @Override
     public String render() {
@@ -21,7 +53,8 @@ public class Home extends Page {
         System.out.println("0 List all cars for sale, 1 go to my page, 2 exit");
         Scanner input = new Scanner(System.in);
         while(true){
-            int action = input.nextInt();
+            int actionCode = input.nextInt();
+            int action = getAction(actionCode);
             switch(action){
                 case LIST:
                     System.out.println("Listing cars");
@@ -39,7 +72,7 @@ public class Home extends Page {
             if(carListCursor == 0){
                 System.out.println("0 List all cars for sale, 1 go to my page");
             } else {
-                System.out.println("0 go to next page, 1 go to previous page, 2  go to my page, 3 ")
+                System.out.println("0 go to next page, 1 go to previous page, 2  go to my page, 3  go to a car by vin");
             }
         }
         return "home";
